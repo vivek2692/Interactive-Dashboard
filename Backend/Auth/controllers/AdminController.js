72206d7899c0f0b756771ff2859211368d91c2be
vivek2692 <79017860+vivek2692@users.noreply.github.com>
2023-02-22 03:21:31 -0800

    mail for otp included

--- Backend/Auth/controllers/AdminController.js
+++ Backend/Auth/controllers/AdminController.js
@@ -1,6 +1,7 @@
 const jwt = require("jsonwebtoken");
 const Admin = require("../models/adminModel.js");
 const bcrypt = require("bcrypt");
+const nodemailer = require("nodemailer");
 
 //Register Admin
 const AdminRegister = async (req, res) => {
@@ -49,7 +50,7 @@ const AdminLogin = async (req, res) => {
             expiresIn: "5d",
           });
           const { password, otp, ...others } = user._doc;
-          res.send({ status: "success", data: { ...others }, "token": token });
+          res.send({ status: "success", data: { ...others }, token: token });
         } else {
           res.send({ status: "failed", msg: "Email or Password is not valid" });
         }
@@ -64,63 +65,98 @@ const AdminLogin = async (req, res) => {
   }
 };
 
-const AdminForgotPassword = async(req, res) => {
-  const {email} = req.body;
-  
-  if(email){
-    const user = await Admin.findOne({email})
+const AdminForgotPassword = async (req, res) => {
+  const { email } = req.body;
 
-    if(user){
-      const otp = Math.floor(Math.random()*(10000-1000+1)+1000)
+  if (email) {
+    const user = await Admin.findOne({ email });
+
+    if (user) {
+      const otp = Math.floor(Math.random() * (10000 - 1000 + 1) + 1000);
       // console.log(otp)
-      const admin = await Admin.findOneAndUpdate({email:email},{otp:otp},{new:true,runValidators:true})
-      res.send({ status: "success", msg: "OTP sent successfully"});
-    }
-    else{
+      const admin = await Admin.findOneAndUpdate(
+        { email: email },
+        { otp: otp },
+        { new: true, runValidators: true }
+      );
+
+      let testAccount = await nodemailer.createTestAccount();
+
+      const transporter = nodemailer.createTransport({
+        host: "smtp-mail.outlook.com",
+        secureConnection: false, // TLS requires secureConnection to be false
+        port: 587,
+        auth: {
+          user: "cvmtestdemo@outlook.com",
+          pass: "Cvmhackathon",
+        },
+      });
+
+      const mailOptions = {
+        from: '"CVM University" <cvmtestdemo@outlook.com>', // sender address
+        to: `${email}`, // list of receivers
+        subject: "Forgot Password OTP", // Subject line
+        text: `Hello ${user.name}`, // plain text body
+        html: `<b>Your OTP : ${otp}</b>`, // html body
+      };
+      transporter.sendMail(mailOptions, function (error, info) {
+        if (error) {
+          return console.log(error);
+        }
+
+        // res.status(StatusCodes.OK).json({ otpsent: true });
+        res.send({ status: "success", msg: "OTP sent successfully" });
+      });
+    } else {
       res.send({ status: "failed", msg: "Please provide valid email" });
     }
-  } else{
+  } else {
     res.send({ status: "failed", msg: "Please provide email" });
   }
-}
+};
 
-const AdminValidateOTP = async(req, res) => {
-  const {email, otp} = req.body;
-  
-  if(email && otp){
-    const admin = await Admin.findOne({email});
+const AdminValidateOTP = async (req, res) => {
+  const { email, otp } = req.body;
 
-    if(admin){
-      if(otp !== admin.otp){
+  if (email && otp) {
+    const admin = await Admin.findOne({ email });
+
+    if (admin) {
+      if (otp !== admin.otp) {
         res.send({ status: "failed", msg: "Please provide valid OTP" });
-      }
-      else{
+      } else {
         res.send({ status: "success", msg: "OTP matched" });
       }
-    }
-    else{
+    } else {
       res.send({ status: "failed", msg: "Please provide valid email" });
     }
-
-  }
-  else{
+  } else {
     res.send({ status: "failed", msg: "All fields are required" });
   }
-}
+};
 
-const AdminUpdatePassword = async(req, res) => {
-  const {email, password} = req.body;
+const AdminUpdatePassword = async (req, res) => {
+  const { email, password } = req.body;
 
   const salt = await bcrypt.genSalt(10);
   const hashedPassword = await bcrypt.hash(password, salt);
 
-  if(email && password){
-    const admin = await Admin.findOneAndUpdate({email}, {password: hashedPassword},{ runValidators: true, new: true, setDefaultsOnInsert: true })
+  if (email && password) {
+    const admin = await Admin.findOneAndUpdate(
+      { email },
+      { password: hashedPassword },
+      { runValidators: true, new: true, setDefaultsOnInsert: true }
+    );
     res.send({ status: "success", msg: "Password Changed successfully" });
-  }
-  else{
+  } else {
     res.send({ status: "failed", msg: "All fields are required" });
   }
-}
+};
 
-module.exports = { AdminRegister, AdminLogin, AdminForgotPassword, AdminValidateOTP, AdminUpdatePassword };
+module.exports = {
+  AdminRegister,
+  AdminLogin,
+  AdminForgotPassword,
+  AdminValidateOTP,
+  AdminUpdatePassword,
+};
