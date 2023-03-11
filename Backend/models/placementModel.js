630de3a73353e4a80c9a22d6939bc88655ab7bd4
manan-2702 <mananrajpara2002@gmail.com>
2023-03-11 13:30:18 +0530

    placementSchema

---
+++ Backend/models/placementModel.js
@@ -0,0 +1,49 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema();
+
+const placementSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  address: {
+    type: String,
+    required: true,
+  },
+  type: {
+    type: String,
+    required: true,
+  },
+  position: {
+    type: String,
+    required: true,
+  },
+  salary: {
+    type: Number,
+    required: true,
+  },
+  college: {
+    type: String,
+    required: true,
+  },
+  department: {
+    type: String,
+    required: true,
+  },
+  year: {
+    type: Number,
+    required: true,
+  },
+  totalPlaced: {
+    type: Number,
+    required: true,
+  },
+  studentId: {
+    type: Schema.Types.ObjectId,
+    ref: "Student",
+    required: true,
+  },
+});
+
+module.exports = mongoose.model("Placement", placementSchema);
