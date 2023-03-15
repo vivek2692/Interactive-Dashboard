08b4344a8f27e5a6d22441d8435442375b236a62
vivek2692 <79017860+vivek2692@users.noreply.github.com>
2023-03-15 01:20:30 -0700

    placement integrated

--- Frontend/src/Admin/Placement-details/admin-placement-details.js
+++ Frontend/src/Admin/Placement-details/admin-placement-details.js
@@ -1,17 +1,48 @@
-import React, { useState, useMemo } from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
-import { Users } from "../../Data/user-info";
+// import { Users } from "../../Data/user-info";
 import { TfiSearch } from "react-icons/tfi";
 import "../CSS/admin-placement-details.css";
 import "../CSS/admin.css";
 import AdminNavBar from "../NavBar/admin-navbar";
 import AdminTopBar from "../TopBar/admin-topbar";
+import axios from 'axios';
 //import MaterialReactTable from "material-react-table";
 
 function AdminPlacement() {
   const [college, setCollege] = useState("");
   const [department, setDepartment] = useState("");
   const [year, setYear] = useState("");
+  const [users, setUsers] = useState([]);
+  const [search, setSearch] = useState("");
+
+  useEffect(() => {
+    const fetchData = async() => {
+      await axios.get(`http://localhost:8000/api/faculty/placement/all-placements?college=${college}&department=${department}&placement_year=${year}`)
+      .then((res) => {
+        const data = res.data.data;
+        setUsers(data);
+      })
+      .catch((err) => {
+        console.log(err);
+      })
+    }
+
+    fetchData();
+  },[college, department, year]);
+
+  const handleSearch = async(e) => {
+    setSearch(e.target.value);
+    await axios.get(`http://localhost:8000/api/faculty/placement/search?enrollment_no=${e.target.value}`)
+    .then((res) => {
+      const data = res.data.data;
+      setUsers(data);
+    })
+    .catch((err) => {
+      console.log(err);
+    })
+  }
+
   let srno = 1;
   return (
     <div className="admin-page">
@@ -30,6 +61,7 @@ function AdminPlacement() {
                     <option value="" disabled selected>
                       --Select College--
                     </option>
+                    <option value="">ALL</option>
                     <option value="GCET">GCET</option>
                     <option value="ADIT">ADIT</option>
                     <option value="MBIT">MBIT</option>
@@ -41,6 +73,7 @@ function AdminPlacement() {
                     <option value="" disabled selected>
                       --Select Department--
                     </option>
+                    <option value="">ALL</option>
                     <option value="CP">CP</option>
                     <option value="IT">IT</option>
                     <option value="ME">ME</option>
@@ -54,6 +87,8 @@ function AdminPlacement() {
                     <option value="" disabled selected>
                       --Select Year--
                     </option>
+                    <option value="">ALL</option>
+                    <option value="2023">2023</option>
                     <option value="2022">2022</option>
                     <option value="2021">2021</option>
                     <option value="2020">2020</option>
@@ -68,6 +103,7 @@ function AdminPlacement() {
                 <input
                   type="text"
                   placeholder="Search by Name or Enrollment no."
+                  onChange={handleSearch}
                 />
                 <button className="search">
                   <TfiSearch style={{ color: "white" }} />
@@ -88,20 +124,20 @@ function AdminPlacement() {
                 <th>Year</th>
                 <th>Action</th>
               </tr>
-              {Users.map((user) => {
+              {users.map((user) => {
                 return (
                   <tr key={srno++}>
                     <td>{srno}</td>
-                    <td>{user.name}</td>
-                    <td>{user.enroll}</td>
+                    <td>{user.student_name}</td>
+                    <td>{user.enrollment_no}</td>
                     <td>{user.department}</td>
                     <td>{user.college}</td>
                     <td>{user.package}</td>
                     <td>{user.company}</td>
-                    <td>{user.year}</td>
+                    <td>{user.placement_year}</td>
                     <td id="table-actions">
                       <Link
-                        to={`/admin/placement-details/student/${user.enroll}`}
+                        to={`/admin/placement-details/student/${user.enrollment_no}`}
                       >
                         <button className="view">
                           <center>Modify</center>
--- Frontend/src/Admin/Placement-details/placement-info.js
+++ Frontend/src/Admin/Placement-details/placement-info.js
@@ -1,22 +1,83 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router";
-import { Users } from "../../Data/user-info";
+// import { Users } from "../../Data/user-info";
 import AdminNavBar from "../NavBar/admin-navbar";
 import AdminTopBar from "../TopBar/admin-topbar";
 import "../CSS/admin.css";
+import axios from "axios";
 
 function ViewPlacement() {
   const { id } = useParams();
 
   let key = 1;
-  const student = Users.find((user) => user.enroll === parseInt(id));
-  const [year, setYear] = useState(student.year);
-  const [college, setCollege] = useState(student.college);
-  const [department, setDepartment] = useState(student.department);
-  const [Package, setPackage] = useState(student.package);
-  const [company, setCompany] = useState(student.company);
+  // const student = Users.find((user) => user.enroll === parseInt(id));
+  const [year, setYear] = useState("");
+  const [college, setCollege] = useState("");
+  const [department, setDepartment] = useState("");
+  const [Package, setPackage] = useState("");
+  const [company, setCompany] = useState("");
   const [duration, setDuration] = useState();
   const [compState, setCompState] = useState("");
+  const [student, setStudent] = useState([]);
+  const [isExist, setIsExist] = useState(false);
+
+  useEffect(() => {
+    const fetchStd = async () => {
+      await axios
+        .get(`http://localhost:8000/api/faculty/placement/placement-info/${id}`)
+        .then((res) => {
+          const data = res.data.data;
+          console.log(data);
+          setStudent(data);
+          setYear(data.placement_year);
+          setCollege(data.college);
+          setDepartment(data.department);
+          setPackage(data.package);
+          setCompany(data.company);
+          setDuration(data.contract_duration);
+          setCompState(data.company_state);
+          setIsExist(true);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    };
+
+    fetchStd();
+  }, []);
+
+  const handleUpdate = async () => {
+    // setIsExist(false);
+    const obj = {
+      placement_year: year,
+      college: college,
+      department: department,
+      package: Package,
+      company: company,
+      contract_duration: duration,
+      company_state: compState
+    };
+    await axios
+      .patch(
+        `http://localhost:8000/api/faculty/placement/update-students/${id}`,
+        obj
+      )
+      .then((res) => {
+        const data = res.data.data;
+        setStudent(data);
+        setYear(data.placement_year);
+        setCollege(data.college);
+        setDepartment(data.department);
+        setPackage(data.package);
+        setCompany(data.company);
+        setDuration(data.contract_duration);
+        setCompState(data.company_state);
+        setIsExist(true);
+      })
+      .catch((err) => {
+        console.log(err);
+      })
+  };
 
   return (
     <div className="admin-page">
@@ -24,52 +85,53 @@ function ViewPlacement() {
       <AdminNavBar />
       <AdminTopBar />
       <div>
-        <div className="student-info-page">
-          <div className="stud-dtl">
-            {/* <div> */}
+        {isExist && (
+          <div className="student-info-page">
+            <div className="stud-dtl">
+              {/* <div> */}
               <div className="info-container">
                 <img
                   width={250}
                   src="https://png.pngtree.com/png-vector/20191101/ourmid/pngtree-cartoon-color-simple-male-avatar-png-image_1934459.jpg"
                 />
-                <h3 color="black">{student.name}</h3>
-                <p color="gray">{student.enroll}</p>
+                <h3 color="black">{student.student_name}</h3>
+                <p color="gray">{student.enrollment_no}</p>
               </div>
               <div className="info-container">
                 <h2>Personal Details</h2>
                 <table className="info-table">
                   <tr>
                     <td>Name : </td>
-                    <td>{student.name}</td>
+                    <td>{student.student_name}</td>
                   </tr>
                   <tr>
                     <td>Email : </td>
-                    <td></td>
+                    <td>{student.student_email}</td>
                   </tr>
                   <tr>
                     <td>Mobile no. : </td>
-                    <td></td>
+                    <td>{student.mobile_number}</td>
                   </tr>
                   <tr>
                     <td>Gender : </td>
-                    <td></td>
+                    <td>{student.gender}</td>
                   </tr>
                   <tr>
                     <td>Enrollment no. : </td>
-                    <td>{student.enroll}</td>
+                    <td>{student.enrollment_no}</td>
                   </tr>
                   <tr>
                     <td>Address : </td>
-                    <td></td>
+                    <td>{student.student_address}</td>
                   </tr>
                   <tr>
                     <td>State : </td>
-                    <td></td>
+                    <td>{student.state}</td>
                   </tr>
                 </table>
               </div>
-            {/* </div> */}
-            {/* <div> */}
+              {/* </div> */}
+              {/* <div> */}
               <div className="info-container" style={{ height: "70vh" }}>
                 <h2>Placement Details</h2>
                 <table className="info-table">
@@ -81,6 +143,8 @@ function ViewPlacement() {
                         onChange={(e) => setYear(e.target.value)}
                       >
                         <option value="">--Select Year--</option>
+                        <option value={2024}>2024</option>
+                        <option value={2023}>2023</option>
                         <option value={2022}>2022</option>
                         <option value={2021}>2021</option>
                         <option value={2020}>2020</option>
@@ -184,12 +248,15 @@ function ViewPlacement() {
                   >
                     Clear
                   </button>
-                  <button className="multistep-form-btn">Save</button>
+                  <button className="multistep-form-btn" onClick={handleUpdate}>
+                    Save
+                  </button>
                 </center>
               </div>
-            {/* </div> */}
+              {/* </div> */}
+            </div>
           </div>
-        </div>
+        )}
       </div>
     </div>
   );
